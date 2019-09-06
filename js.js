49e87d0e83a1c12c72632feb91a64da86ce626f8
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-09-06 17:04:56 +0530

    bug fixes

--- js.js
+++ js.js
@@ -1,92 +1,79 @@
-var scroll = 0;
-var oldScroll = -1;
 window.addEventListener("scroll", function(event){
-    oldScroll = scroll;
-    scroll = this.scrollY;
-    var dir = 1;
-    scroll > oldScroll ? dir = 1 : dir = -1;
-    
+    var scroll = this.scrollY;
+    scroll = scroll+30;
 //    console.log(scroll)
-    update(dir);
+    drawAndUpdate(scroll/10.0);
 })
+var numCircles = 5000;
 
-var numberOfCircles = 500;
-
-var mainCanvas = document.getElementById("canvas");
-var mainContext = mainCanvas.getContext('2d');
-var size = 2000;
-mainCanvas.width = size * 2;
-mainCanvas.height = size * 2;
-mainCanvas.style.width = size/2;
-mainCanvas.style.height = size/2;
+var canvas = document.getElementById("circle");
+var mainContext = canvas.getContext('2d');
+var size = 1000;
+canvas.width = size * 2;
+canvas.height = size * 2;
+canvas.style.width = size/2;
+canvas.style.height = size/2;
 var circ = Math.PI * 2;
 var quart = Math.PI / 2;
  
 var circles = [];
 
-var Circle = function(r, x, y) {
+var Circle = function(radius, width, xPos, yPos) {
   this.speed = Math.random() * 0.001
-  this.r = r;
-  this.x = x;
-  this.y = y;
-  this.iter = 0;
+  this.width = width;
+  this.radius = radius;
+  this.xPos = xPos;
+  this.yPos = yPos;
   this.offset = 0.001;
-  this.animationSpeed = 0.02;
-  this.counter = Math.random() * 360;
+  this.counter = Math.random() * 400;
   this.lineWidth = 2;
-  var R = Math.round(Math.random() * 55 + 200),
-      G = Math.round(Math.random() * 55 + 200),
-      B = Math.round(Math.random() * 55 + 200),
+  var R = Math.round(Math.random() * 35 + 190),
+      G = Math.round(Math.random() * 35 + 190),
+      B = Math.round(Math.random() * 35 + 190),
       O = Math.random() * 1;
   this.color = ['rgba(' + R, G, B, O + ')'].join(',');
 };
 
-Circle.prototype.update = function(dir) {
+Circle.prototype.update = function(scroll_pos) {
+  this.counter += Math.random() * this.speed;
+  
   mainContext.beginPath();
-  var curPos = (circ) * (this.counter) + quart;
-  if(curPos < (curPos - this.offset)){
-     mainContext.arc(this.x, 
-                  this.y, 
-                  this.r,
+  var curPos = -((circ) * (this.counter/100)) - quart;
+  mainContext.arc(this.xPos, 
+                  this.yPos, 
+                  this.radius, 
+                  curPos - scroll_pos*this.offset, 
                   curPos,
-                  curPos-this.offset, 
                   false);
-  }else{
-      mainContext.arc(this.x, 
-                      this.y, 
-                      this.r,
-                      curPos-this.offset,
-                      curPos, 
-                      false);
-  }
-
   mainContext.lineWidth = this.lineWidth;
   mainContext.strokeStyle = this.color;
-
+//  this.offset += 0.00001;
   mainContext.stroke();
-  this.offset = this.offset + dir*this.animationSpeed;
 };
 
 var setupCircles = function() {
-  var X = window.innerWidth;
-  var Y = window.innerHeight;
+  var centerX = canvas.width / 4;
+  var centerY = 0; // canvas.height / 2;
   
-  for (var n = 0; n < numberOfCircles; n++) {
-    var radius = Math.random() * (mainCanvas.width+mainCanvas.height)/2;
+  for (var n = 0; n < numCircles; n++) {
+    var radius = Math.random() * canvas.width;
 
-    var circle = new Circle(radius, X, Y);
+    var circle = new Circle(radius, 400, centerX, centerY);
     circles.push(circle);
-  }    
+  }
   
+  drawAndUpdate(3);
 }
 
-var update = function(dir) { 
-  mainContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+var drawAndUpdate = function(scroll_pos) { 
+  mainContext.clearRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < circles.length; i++) {
     var myCircle = circles[i];
-    myCircle.update(dir);
+    myCircle.update(scroll_pos);
   }
 }
 
 
 setupCircles();
+
+
