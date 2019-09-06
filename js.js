520f75ddb65cecd622315b8ee9e5596cd40da499
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-09-06 20:44:30 +0530

    Personal website details

--- js.js
+++ js.js
@@ -1,10 +1,38 @@
+function isElementInViewport(el) {
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (7 * window.innerHeight / 8 || 7 * document.documentElement.clientHeight / 8) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+var sections = document.getElementsByClassName('spread');
+
+function animation(el, class_name) {
+    for (var i = 0; i < el.length; i++) {
+        if (isElementInViewport(el[i])) {
+            el[i].classList.add(class_name);
+        }
+    }
+    
+}
+animation(sections, 'spread-animate');
 window.addEventListener("scroll", function(event){
     var scroll = this.scrollY;
-    scroll = scroll+30;
+    scroll = scroll+5;
 //    console.log(scroll)
-    drawAndUpdate(scroll/10.0);
+    drawAndUpdate(scroll/2.0);
+    animation(sections, 'spread-animate');
 })
-var numCircles = 5000;
+var numCircles = 1000;
 
 var canvas = document.getElementById("circle");
 var mainContext = canvas.getContext('2d');
@@ -26,11 +54,11 @@ var Circle = function(radius, width, xPos, yPos) {
   this.yPos = yPos;
   this.offset = 0.001;
   this.counter = Math.random() * 400;
-  this.lineWidth = 2;
+  this.lineWidth = 1;
   var R = Math.round(Math.random() * 35 + 190),
       G = Math.round(Math.random() * 35 + 190),
       B = Math.round(Math.random() * 35 + 190),
-      O = Math.random() * 1;
+      O = Math.random() * 0.3;
   this.color = ['rgba(' + R, G, B, O + ')'].join(',');
 };
 
@@ -53,7 +81,7 @@ Circle.prototype.update = function(scroll_pos) {
 
 var setupCircles = function() {
   var centerX = canvas.width / 4;
-  var centerY = 0; // canvas.height / 2;
+  var centerY = canvas.height / 4;
   
   for (var n = 0; n < numCircles; n++) {
     var radius = Math.random() * canvas.width;
