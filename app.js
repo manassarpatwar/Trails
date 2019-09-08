9146d6feba6bad636e6fcf69fb4eba577090d14f
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-09-08 23:17:25 +0530

    Performance improvements

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ function animation(el, class_name) {
 }
 
 var randomQuotes = document.getElementsByClassName("random-splash");
-var rand = Math.round(Math.random()*(randomQuotes.length-1));
-console.log(rand)
+var rand = Math.round(Math.random() * (randomQuotes.length - 1));
 randomQuotes[rand].style.display = "block";
 
 var sections = document.getElementsByClassName('spread');
@@ -34,142 +33,16 @@ window.addEventListener("scroll", function (event) {
     animation(sections, 'spread-animate');
 })
 
-var canvas = document.getElementById("canvas");
 
-context = canvas.getContext("2d");
-var h = 0;
+var backgroundGradients = ["linear-gradient(to bottom, #FF3CAC 0%, #562B7C 52%, #2B86C5 100%)",
+    "linear-gradient(to top, #09203f 0%, #537895 100%)",
+    "linear-gradient(to top, #f43b47 0%, #453a94 100%)",
+    "linear-gradient(to top, #3ab5b0 0%, #3d99be 31%, #56317a 100%)",
+    "linear-gradient(to bottom, #243949 0%, #517fa4 100%)",
+    "linear-gradient(to bottom, #3d3393 0%, #2b76b9 70%, #2cacd1 100%)",
+    "linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%)",
+    "linear-gradient(-20deg, #2b5876 0%, #4e4376 100%)",
+    "radial-gradient(black 0%, black 100%)"]
+var rand = Math.round(Math.random() * (backgroundGradients.length - 1));
+canvas.style.backgroundImage = backgroundGradients[rand];
 
-function resizeCanvas() {
-    var w = window.innerWidth;
-    if (window.innerHeight > h)
-        h = window.innerHeight;
-    canvas.width = w;
-    canvas.height = h;
-    canvas.style.width = w + "px";
-    canvas.style.height = h + "px";
-    context.translate(w / 2, h / 2);
-};
-
-resizeCanvas();
-window.onresize = resizeCanvas;
-
-
-
-
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-//context.translate(centerX, centerY);
-//var half_degree = Math.PI / 720;
-var circle_radius = 2.4;
-var offset = 0.01;
-var numTrails = canvas.width / 10;
-var trails = [];
-var minimumRadius = 0.6;
-
-function setup() {
-    for (var i = 0; i < numTrails; i++)
-        trails.push(new Trail());
-}
-setup();
-
-function Trail() {
-    this.circles = [];
-    this.starOpacity = Math.random() - 0.2;
-    //    this.R = Math.round(Math.random() * 35 + 220),
-    //    this.G = Math.round(Math.random() * 35 + 220),
-    //    this.B = Math.round(Math.random() * 35 + 220),
-    this.circles.push(new Circle(Math.random() * canvas.width - canvas.width / 2, Math.random() * canvas.height * 2 - canvas.height, circle_radius));
-}
-
-function Circle(x, y, r) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-}
-
-Circle.prototype.setX = function (newX) {
-    this.x = newX;
-}
-
-Circle.prototype.setY = function (newY) {
-    this.y = newY;
-}
-
-Circle.prototype.setR = function (newR) {
-    this.r = newR;
-}
-
-
-
-
-function updateStarCoordinates(trail_index) {
-    var trail = trails[trail_index];
-    var circle = trail.circles[trail.circles.length - 1];
-    var radius = Math.sqrt(Math.pow((circle.x - 0), 2) + Math.pow((circle.y - 0), 2));
-    var atan = Math.atan2(circle.y, circle.x);
-    //    console.log(atan);
-    var dTheta = 1 / radius;
-    var newX = radius * Math.cos(atan - dTheta);
-    var newY = radius * Math.sin(atan - dTheta);
-
-    var prevX = circle.x;
-    var prevY = circle.y;
-    var prevR = circle.r;
-    circle.setX(newX);
-    circle.setY(newY);
-    circle.setR(prevR);
-
-    updateTrailCoordinates(trail_index, trail.circles.length - 2, prevX, prevY, circle.r - 50 * offset)
-}
-
-function updateTrailCoordinates(trail_index, index, x, y, r) {
-    if (index >= 0) {
-        var circle = trails[trail_index].circles[index];
-        var prevX = circle.x;
-        var prevY = circle.y;
-        var prevR = circle.r;
-        circle.setX(x);
-        circle.setY(y);
-        circle.setR(r);
-
-        updateTrailCoordinates(trail_index, index - 1, prevX, prevY, prevR - offset);
-    }
-}
-
-function update() {
-    context.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    for (var i = 0; i < numTrails; i++) {
-        if (trails[i].circles[0].r > minimumRadius)
-            trails[i].circles.unshift(new Circle(0, 0, 0));
-        updateStarCoordinates(i);
-        drawTrail(i);
-    }
-    requestAnimationFrame(update);
-}
-
-
-function drawTrail(trail_index) {
-    var trail = trails[trail_index];
-    for (var i = 0; i < trail.circles.length; i++) {
-        var c = trail.circles[i];
-        if ((c.x > canvas.width / 2 || c.x < -canvas.width / 2) &&
-            c.y > canvas.height / 2 || c.y < -canvas.height / 2) {
-            continue;
-        }
-        context.beginPath();
-        var O;
-
-        if (i != trails[trail_index].circles.length - 1) {
-            O = trail.starOpacity - c.r / 20;
-            context.rect(c.x - c.r / 2, c.y - c.r / 2, c.r, c.r);
-        } else {
-            context.arc(c.x, c.y, c.r, 0, Math.PI * 2);
-            O = trail.starOpacity;
-        }
-        //        var color = ['rgba(' + trail.R, trail.G, trail.B, O + ')'].join(',');
-        var color = "rgba(255, 255, 255, " + O + ")";
-        context.fillStyle = color;
-        context.fill();
-    }
-}
-update();
---
+++ trails_arcs.js
@@ -0,0 +1,95 @@
+var canvas = document.getElementById("canvas");
+
+context = canvas.getContext("2d");
+var h = 0;
+
+function resizeCanvas() {
+    var w = window.innerWidth;
+    if (window.innerHeight > h)
+        h = window.innerHeight;
+    canvas.width = w;
+    canvas.height = h;
+    canvas.style.width = w + "px";
+    canvas.style.height = h + "px";
+    context.translate(w / 2, h / 2);
+};
+
+resizeCanvas();
+window.onresize = resizeCanvas;
+
+var half_degree = Math.PI / 180;
+
+var offset = 0.05;
+var star_radius = 3;
+var trails = [];
+var numTrails = 100;
+var speed = Math.PI/400;
+
+function setup(){
+    for(var i = 0; i < numTrails; i++){
+        var radius =  Math.sqrt(Math.pow(Math.random()*canvas.width - canvas.width/2 , 2) + Math.pow(Math.random()*canvas.height - canvas.height/2, 2));
+        trails.push(new Trail(radius))
+    }
+}
+setup();
+
+function Trail(radius){
+    this.arcs = [];
+    this.arc_width = 2;
+    this.radius = radius;
+    this.st_ang = Math.random()*Math.PI*2;
+    this.starOpacity = (Math.random()+0.5)/1.6667;
+}
+
+function Arc(start_angle, end_angle, width, radius) {
+    this.start_angle = start_angle;
+    this.end_angle = end_angle;
+    this.width = width;
+    this.radius = radius;
+}
+
+//window.addEventListener("scroll", function () {
+function animate(){
+    context.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    for(var i = 0; i < numTrails; i++){
+        if (trails[i].arc_width >= 0) {
+//            var st_ang = Math.random()*Math.PI*2;
+            trails[i].arcs.push(new Arc(trails[i].st_ang, trails[i].st_ang+half_degree, trails[i].arc_width, trails[i].radius));
+        }
+        changeStarParams(i, trails[i].arcs.length - 1);
+        drawTrails(i);
+        trails[i].arc_width -= offset;
+    }
+    requestAnimationFrame(animate);
+}
+animate();
+
+function changeStarParams(trail_index, arc_index) {
+    if (arc_index >= 0) {
+        var arc = trails[trail_index].arcs[arc_index];
+        arc.end_angle = arc.start_angle;
+        arc.start_angle -= speed;
+        changeStarParams(trail_index, arc_index - 1);
+    }
+}
+
+function drawTrails(trail_index) {
+    var trail = trails[trail_index];
+    var arc = trail.arcs[0];
+    var X = arc.radius * Math.cos(arc.start_angle);
+    var Y = arc.radius * Math.sin(arc.start_angle);
+    context.beginPath();
+    context.arc(X, Y, star_radius, 0, Math.PI * 2);
+    context.fillStyle = "rgba(255, 255, 255, " + trail.starOpacity + ")";
+    context.fill();
+    for (var i = 0; i < trail.arcs.length; i++) {
+        var arc = trail.arcs[i];
+        context.beginPath();
+        context.arc(0, 0, arc.radius, arc.start_angle, arc.end_angle);
+        context.lineWidth = arc.width;
+        var O = trail.starOpacity - arc.width/20;
+        var color = "rgba(255, 255, 255, " + O + ")";
+        context.strokeStyle = color;
+        context.stroke();
+    }
+}
