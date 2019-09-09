c31f5bce63a020934c601fa355a9aaba08840310
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-09-09 10:11:44 +0530

    Improvements

--- trails_arcs.js
+++ trails_arcs.js
@@ -1,44 +1,42 @@
 var canvas = document.getElementById("canvas");
-
-context = canvas.getContext("2d");
-var h = 0;
+var context = canvas.getContext("2d");
+var w;
+var h;
 
 function resizeCanvas() {
-    var w = window.innerWidth;
-    if (window.innerHeight > h)
-        h = window.innerHeight;
-    canvas.width = w;
-    canvas.height = h;
-    canvas.style.width = w + "px";
-    canvas.style.height = h + "px";
-    context.translate(w / 2, h / 2);
-};
-
+    canvas.width = window.innerWidth*2;
+    canvas.height = 1500;
+    w = canvas.width;
+    h = canvas.height
+    canvas.style.width = w/2 + "px";
+    canvas.style.height = h/2 + "px";
+    context.translate(w/2,h/2);
+}
 resizeCanvas();
 window.onresize = resizeCanvas;
 
 var half_degree = Math.PI / 180;
 
-var offset = 0.05;
-var star_radius = 3;
+var offset = 0.1;
+var star_radius = 2.2;
 var trails = [];
-var numTrails = 100;
-var speed = Math.PI/400;
+var numTrails = 200;
+var speed = Math.PI / 360;
 
-function setup(){
-    for(var i = 0; i < numTrails; i++){
-        var radius =  Math.sqrt(Math.pow(Math.random()*canvas.width - canvas.width/2 , 2) + Math.pow(Math.random()*canvas.height - canvas.height/2, 2));
+function setup() {
+    for (var i = 0; i < numTrails; i++) {
+        var radius = Math.sqrt(Math.pow(Math.random() * w - w / 2, 2) + Math.pow(Math.random() * h - h / 2, 2));
         trails.push(new Trail(radius))
     }
 }
 setup();
 
-function Trail(radius){
+function Trail(radius) {
     this.arcs = [];
-    this.arc_width = 2;
+    this.arc_width = 5;
     this.radius = radius;
-    this.st_ang = Math.random()*Math.PI*2;
-    this.starOpacity = (Math.random()+0.5)/1.6667;
+    this.st_ang = Math.random() * Math.PI * 2;
+    this.starOpacity = (Math.random() + 0.5) / 1.6667;
 }
 
 function Arc(start_angle, end_angle, width, radius) {
@@ -49,12 +47,11 @@ function Arc(start_angle, end_angle, width, radius) {
 }
 
 //window.addEventListener("scroll", function () {
-function animate(){
-    context.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    for(var i = 0; i < numTrails; i++){
+function animate() {
+    context.clearRect(-w / 2, -h / 2, w, h);
+    for (var i = 0; i < trails.length; i++) {
         if (trails[i].arc_width >= 0) {
-//            var st_ang = Math.random()*Math.PI*2;
-            trails[i].arcs.push(new Arc(trails[i].st_ang, trails[i].st_ang+half_degree, trails[i].arc_width, trails[i].radius));
+            trails[i].arcs.push(new Arc(trails[i].st_ang, trails[i].st_ang + half_degree, trails[i].arc_width, trails[i].radius));
         }
         changeStarParams(i, trails[i].arcs.length - 1);
         drawTrails(i);
@@ -78,18 +75,25 @@ function drawTrails(trail_index) {
     var arc = trail.arcs[0];
     var X = arc.radius * Math.cos(arc.start_angle);
     var Y = arc.radius * Math.sin(arc.start_angle);
-    context.beginPath();
-    context.arc(X, Y, star_radius, 0, Math.PI * 2);
-    context.fillStyle = "rgba(255, 255, 255, " + trail.starOpacity + ")";
-    context.fill();
-    for (var i = 0; i < trail.arcs.length; i++) {
-        var arc = trail.arcs[i];
+
+    if ((X >= -w / 2 || X <= w / 2) && (Y >= -h / 2 || Y <= h / 2)) {
         context.beginPath();
-        context.arc(0, 0, arc.radius, arc.start_angle, arc.end_angle);
-        context.lineWidth = arc.width;
-        var O = trail.starOpacity - arc.width/20;
-        var color = "rgba(255, 255, 255, " + O + ")";
-        context.strokeStyle = color;
-        context.stroke();
+        context.arc(X, Y, star_radius, 0, Math.PI * 2);
+        context.fillStyle = "rgba(255, 255, 255, " + trail.starOpacity + ")";
+        context.fill();
+    }
+    for (var i = 0; i < trail.arcs.length; i++) {
+        arc = trail.arcs[i];
+        var x = arc.radius * Math.cos(arc.start_angle);
+        var y = arc.radius * Math.sin(arc.start_angle);
+        if ((x >= -w / 2 || x <= w / 2) && (y >= -h / 2 || y <= h / 2)) {
+            context.beginPath();
+            context.arc(0, 0, arc.radius, arc.start_angle, arc.end_angle);
+            context.lineWidth = arc.width;
+            var O = trail.starOpacity - arc.width / 20;
+            var color = "rgba(255, 255, 255, " + O + ")";
+            context.strokeStyle = color;
+            context.stroke();
+        }
     }
 }
